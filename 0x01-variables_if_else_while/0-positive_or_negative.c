45eefaef1a8713d8e48c22e8f8b56be8dc507221
KirukuTZ <wakilikiruku690@gmail.com>
2022-07-04 22:12:11 -0700

    all

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,8 +14,16 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 /* your code goes there */
 if (n > 0)
-printf("%d is positive\n",n);
+{	
+printf("%d is %s\n", n, "positive");
+}
 else if (n < 0)
-printf("%d is zero\n",n);
+{
+printf("%d is %s\n", n, "negative");
+}
+else
+{
+printf("%d is %s\n, n, "zero");
+}
 return (0);
 }
